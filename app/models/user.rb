b55e74dc818884b6c86a8f0c92b1c6376bf35cab
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2022-09-22 20:29:54 -0700

    AttributeProtector concern and spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@
 
 # Represents individual users that can authenticate with the application.
 class User < ApplicationRecord
+  include AttributeProtector
+
   has_secure_token :confirmation_token
   has_secure_password
+
+  protect_attributes :confirmation_token, :password, :password_confirmation, :password_digest
 end
